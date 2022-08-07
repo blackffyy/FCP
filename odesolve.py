e13e75a6a8def3a3554441b77a2c060a73d31952
blackffyy <jc21171@bristol.ac.uk>
2022-08-07 15:18:57 +0800

    main function of the program 6 pass 5

--- odesolve.py
+++ odesolve.py
@@ -9,16 +9,41 @@
 
 def euler(f, x, t, h):
     """Perform one step of the Euler method"""
+#Eular Method    
+    return x + f (x , t )*h
     pass
 
 
 def rk4(f, x, t, h):
     """Perform one step of the RK$ method"""
+    k1 = f(x,t)
+    k2 = f(x+k1*(h/2),t+(h/2)) 
+    k3 = f(x+k2*(h/2),t+(h/2)) 
+    k4 = f(x+k3*h,t+h)
+    return x + ( k1 + 2*k2 + 2*k3 + k4 ) * h/6
     pass
 
 
 def solveto(f, x1, t1, t2, hmax, method=euler):
     """Use many steps of method to get from x1,t1 to x2,t2"""
+    loop = (t2 - t1)/hmax 
+    loop = int(round(loop))
+    Damonsb = x1
+    t = t1
+    for i in range(0,loop):
+        if method == rk4:
+            Damonsb = rk4(f, Damonsb, t, hmax)
+        else:
+            Damonsb = euler(f, Damonsb, t, hmax) 
+        t = t+hmax  
+    if t != t2:
+        hmax = t2 - t
+        if method == rk4:
+            Damonsb = rk4(f, Damonsb, t, hmax)
+        else:
+            Damonsb = euler(f, Damonsb, t, hmax) 
+        t = t+hmax  
+    return Damonsb
     pass
 
 
