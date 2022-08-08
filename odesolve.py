716e1ec09ac40e4a12ad62ae34c7211082d0f1e8
blackffyy <jc21171@bristol.ac.uk>
2022-08-08 10:46:46 +0800

    all part of the function that passing 6 test out of 6 and also passing part9(error)

--- odesolve.py
+++ odesolve.py
@@ -66,3 +66,32 @@ def odesolve(f, X0, t, hmax, method=euler):
     YMY = np.array(YMY)
     return YMY 
     pass
+
+#test error
+import numpy as np
+import matplotlib.pyplot as plt
+from odesolve import solveto, euler, rk4
+def f(x, t):
+    return x
+x0 = 1
+t1, t2 = 0, 1
+hmax_array = np.logspace(-5, -1, 100)
+gt_value = np.e
+error_euler, error_rk4 = [], []
+for hmax in hmax_array:
+    guess_euler = solveto(f, x0, t1, t2, hmax, euler)
+    guess_rk4 = solveto(f, x0, t1, t2, hmax, rk4)
+    error_euler.append(gt_value - guess_euler)
+    error_rk4.append(gt_value - guess_rk4)
+#Drawing
+plt.scatter(hmax_array, error_euler, label='Euler', s=5)
+plt.scatter(hmax_array, error_rk4, label='RK4', s=5)
+plt.xscale("log")
+plt.yscale("log")
+#Setting the x axes and y axes
+plt.xlabel("h")
+plt.ylabel("error")
+plt.legend()
+#title
+plt.title('Comparison of errors for Euler and RK4')
+plt.show()
